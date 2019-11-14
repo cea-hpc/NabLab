150568e508d43590f423187a11182f994d9aa773
Marie-Pierre OUDOT <marie-pierre.oudot@cea.fr>
2019-11-14 14:24:46 +0100

    FunctionCall Interpreter

--- fr.cea.nabla.tests/src/fr/cea/nabla/tests/TestFunctions.java
+++ fr.cea.nabla.tests/src/fr/cea/nabla/tests/TestFunctions.java
@@ -2,8 +2,18 @@ package fr.cea.nabla.tests;
 
 public class TestFunctions 
 {
+	static int getOne()
+	{
+		return 1;
+	}
+
 	static int addOne(int a)
 	{
-		return a +1;
+		return a + 1;
+	}
+
+	static int add(int a, int b)
+	{
+		return a + b;
 	}
 }

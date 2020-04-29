c2d42f640bbe9a5d6c14741a93fc8d00a3fb62a1
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2020-04-29 10:49:05 +0200

    C++ generation of example files updated with Options ctr in .cpp file.

--- plugins/fr.cea.nabla.ui/examples/NablaExamples/src-gen-cpp/kokkos/explicitheatequation/ExplicitHeatEquation.h
+++ plugins/fr.cea.nabla.ui/examples/NablaExamples/src-gen-cpp/kokkos/explicitheatequation/ExplicitHeatEquation.h
@@ -50,69 +50,7 @@ public:
 		double option_stoptime;
 		size_t option_max_iterations;
 
-		Options(const std::string& fileName)
-		{
-			ifstream ifs(fileName);
-			rapidjson::IStreamWrapper isw(ifs);
-			rapidjson::Document d;
-			d.ParseStream(isw);
-			assert(d.IsObject());
-			// X_LENGTH
-			assert(d.HasMember("X_LENGTH"));
-			const rapidjson::Value& valueof_X_LENGTH = d["X_LENGTH"];
-			assert(valueof_X_LENGTH.IsDouble());
-			X_LENGTH = valueof_X_LENGTH.GetDouble();
-			// Y_LENGTH
-			assert(d.HasMember("Y_LENGTH"));
-			const rapidjson::Value& valueof_Y_LENGTH = d["Y_LENGTH"];
-			assert(valueof_Y_LENGTH.IsDouble());
-			Y_LENGTH = valueof_Y_LENGTH.GetDouble();
-			// u0
-			assert(d.HasMember("u0"));
-			const rapidjson::Value& valueof_u0 = d["u0"];
-			assert(valueof_u0.IsDouble());
-			u0 = valueof_u0.GetDouble();
-			// vectOne
-			assert(d.HasMember("vectOne"));
-			const rapidjson::Value& valueof_vectOne = d["vectOne"];
-			assert(valueof_vectOne.IsArray());
-			assert(valueof_vectOne.Size() == 2);
-			for (size_t i1=0 ; i1<2 ; i1++)
-			{
-				assert(valueof_vectOne[i1].IsDouble());
-				vectOne[i1] = valueof_vectOne[i1].GetDouble();
-			}
-			// X_EDGE_ELEMS
-			assert(d.HasMember("X_EDGE_ELEMS"));
-			const rapidjson::Value& valueof_X_EDGE_ELEMS = d["X_EDGE_ELEMS"];
-			assert(valueof_X_EDGE_ELEMS.IsInt());
-			X_EDGE_ELEMS = valueof_X_EDGE_ELEMS.GetInt();
-			// Y_EDGE_ELEMS
-			assert(d.HasMember("Y_EDGE_ELEMS"));
-			const rapidjson::Value& valueof_Y_EDGE_ELEMS = d["Y_EDGE_ELEMS"];
-			assert(valueof_Y_EDGE_ELEMS.IsInt());
-			Y_EDGE_ELEMS = valueof_Y_EDGE_ELEMS.GetInt();
-			// X_EDGE_LENGTH
-			assert(d.HasMember("X_EDGE_LENGTH"));
-			const rapidjson::Value& valueof_X_EDGE_LENGTH = d["X_EDGE_LENGTH"];
-			assert(valueof_X_EDGE_LENGTH.IsDouble());
-			X_EDGE_LENGTH = valueof_X_EDGE_LENGTH.GetDouble();
-			// Y_EDGE_LENGTH
-			assert(d.HasMember("Y_EDGE_LENGTH"));
-			const rapidjson::Value& valueof_Y_EDGE_LENGTH = d["Y_EDGE_LENGTH"];
-			assert(valueof_Y_EDGE_LENGTH.IsDouble());
-			Y_EDGE_LENGTH = valueof_Y_EDGE_LENGTH.GetDouble();
-			// option_stoptime
-			assert(d.HasMember("option_stoptime"));
-			const rapidjson::Value& valueof_option_stoptime = d["option_stoptime"];
-			assert(valueof_option_stoptime.IsDouble());
-			option_stoptime = valueof_option_stoptime.GetDouble();
-			// option_max_iterations
-			assert(d.HasMember("option_max_iterations"));
-			const rapidjson::Value& valueof_option_max_iterations = d["option_max_iterations"];
-			assert(valueof_option_max_iterations.IsInt());
-			option_max_iterations = valueof_option_max_iterations.GetInt();
-		}
+		Options(const std::string& fileName);
 	};
 
 	Options* options;
--- plugins/fr.cea.nabla.ui/examples/NablaExamples/src-gen-cpp/stl-thread/heatequation/HeatEquation.cc
+++ plugins/fr.cea.nabla.ui/examples/NablaExamples/src-gen-cpp/stl-thread/heatequation/HeatEquation.cc
@@ -16,6 +16,56 @@ double sumR0(double a, double b)
 }
 
 
+/******************** Options definition ********************/
+
+HeatEquation::Options::Options(const std::string& fileName)
+{
+	ifstream ifs(fileName);
+	rapidjson::IStreamWrapper isw(ifs);
+	rapidjson::Document d;
+	d.ParseStream(isw);
+	assert(d.IsObject());
+	// X_EDGE_LENGTH
+	assert(d.HasMember("X_EDGE_LENGTH"));
+	const rapidjson::Value& valueof_X_EDGE_LENGTH = d["X_EDGE_LENGTH"];
+	assert(valueof_X_EDGE_LENGTH.IsDouble());
+	X_EDGE_LENGTH = valueof_X_EDGE_LENGTH.GetDouble();
+	// Y_EDGE_LENGTH
+	assert(d.HasMember("Y_EDGE_LENGTH"));
+	const rapidjson::Value& valueof_Y_EDGE_LENGTH = d["Y_EDGE_LENGTH"];
+	assert(valueof_Y_EDGE_LENGTH.IsDouble());
+	Y_EDGE_LENGTH = valueof_Y_EDGE_LENGTH.GetDouble();
+	// X_EDGE_ELEMS
+	assert(d.HasMember("X_EDGE_ELEMS"));
+	const rapidjson::Value& valueof_X_EDGE_ELEMS = d["X_EDGE_ELEMS"];
+	assert(valueof_X_EDGE_ELEMS.IsInt());
+	X_EDGE_ELEMS = valueof_X_EDGE_ELEMS.GetInt();
+	// Y_EDGE_ELEMS
+	assert(d.HasMember("Y_EDGE_ELEMS"));
+	const rapidjson::Value& valueof_Y_EDGE_ELEMS = d["Y_EDGE_ELEMS"];
+	assert(valueof_Y_EDGE_ELEMS.IsInt());
+	Y_EDGE_ELEMS = valueof_Y_EDGE_ELEMS.GetInt();
+	// option_stoptime
+	assert(d.HasMember("option_stoptime"));
+	const rapidjson::Value& valueof_option_stoptime = d["option_stoptime"];
+	assert(valueof_option_stoptime.IsDouble());
+	option_stoptime = valueof_option_stoptime.GetDouble();
+	// option_max_iterations
+	assert(d.HasMember("option_max_iterations"));
+	const rapidjson::Value& valueof_option_max_iterations = d["option_max_iterations"];
+	assert(valueof_option_max_iterations.IsInt());
+	option_max_iterations = valueof_option_max_iterations.GetInt();
+	// PI
+	assert(d.HasMember("PI"));
+	const rapidjson::Value& valueof_PI = d["PI"];
+	assert(valueof_PI.IsDouble());
+	PI = valueof_PI.GetDouble();
+	// alpha
+	assert(d.HasMember("alpha"));
+	const rapidjson::Value& valueof_alpha = d["alpha"];
+	assert(valueof_alpha.IsDouble());
+	alpha = valueof_alpha.GetDouble();
+}
 
 /******************** Module definition ********************/
 

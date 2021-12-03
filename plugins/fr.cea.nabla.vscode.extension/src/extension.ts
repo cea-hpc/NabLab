67094e12bca9fb142f9a2193dd0c0dd62c3891a7
Axel RICHARD <axel.richard@obeo.fr>
2021-12-03 11:38:21 +0100

    [dev] VSCode extension: improve LatexView
    Use VSCode theme color to display the latex formula.
    
    Change-Id: I57dedf8f28a56c3e49879ee1a05d527d1cddca66
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ plugins/fr.cea.nabla.vscode.extension/src/extension.ts
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2021 CEA
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * Contributors: see AUTHORS file
+ *******************************************************************************/
+import * as path from 'path';
+import * as os from 'os';
+
+import {Trace} from 'vscode-jsonrpc';
+import { commands, window, workspace, ExtensionContext} from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import LatexWebViewLoader from './view/LatexWebViewLoader';
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/mydsl
+    let launcher = os.platform() === 'win32' ? 'nabla-ls.bat' : 'nabla-ls';
+    let script = context.asAbsolutePath(path.join('src', 'nabla', 'bin', launcher));
+
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        args: ['-trace'],
+        debug: { command: script, args: [], options: { env: createDebugEnv() } }
+    };
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['nabla'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    let lc = new LanguageClient('NabLab Xtext Server', serverOptions, clientOptions);
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.trace = Trace.Verbose;
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+
+	let disposableLatexPanel = commands.registerCommand('nablabweb.showLatexView', () => {
+        LatexWebViewLoader.createOrShow(context.extensionPath);
+    });
+	context.subscriptions.push(disposableLatexPanel);
+
+    window.onDidChangeTextEditorSelection((e) => {
+        if (e?.textEditor?.document?.languageId === 'nabla') {
+            const activeEditorFileURI = e.textEditor.document.uri;
+            const projectFolder = workspace.getWorkspaceFolder(activeEditorFileURI);
+			const offset = e.textEditor.document.offsetAt(e.textEditor.selection?.anchor);
+            if (projectFolder && offset) {
+                LatexWebViewLoader.update(context.extensionPath, projectFolder.name, activeEditorFileURI.fsPath, offset);
+            }
+        }
+    });
+}
+
+export function deactivate() {}
+
+function createDebugEnv() {
+    return Object.assign({
+        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
+    }, process.env)
+}
---
+++ plugins/fr.cea.nabla.vscode.extension/src/view/LatexWebViewLoader.ts
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2021 CEA
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * Contributors: see AUTHORS file
+ *******************************************************************************/
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+export default class LatexWebViewLoader {
+  public static openedPanel: LatexWebViewLoader | undefined;
+
+  private readonly panel: vscode.WebviewPanel;
+
+  public static createOrShow(extensionPath: string) {
+    const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+    const webViewContext = {extensionPath, projectName: '', nablaModelPath: '', offset: 0};
+    // If we already have a panel, show it.
+    if (LatexWebViewLoader.openedPanel) {
+      LatexWebViewLoader.openedPanel.panel.reveal(column);
+      return;
+    }
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel('nablabweb', 'LaTex', column || vscode.ViewColumn.One, {
+      enableScripts: true,
+      localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'nablabweb'))],
+    });
+    LatexWebViewLoader.openedPanel = new LatexWebViewLoader(panel, webViewContext);
+    // Once webview has been created, move it to the bottom of the editors part of the screen.
+    vscode.commands.executeCommand('workbench.action.moveEditorToBelowGroup');
+  }
+
+  public static update(extensionPath: string, projectName: string, nablaModelPath: string, offset: number) {
+    // If we don't have a panel, do nothing.
+    if (!LatexWebViewLoader.openedPanel) {
+      return;
+    }
+    LatexWebViewLoader.openedPanel.panel.webview.html = LatexWebViewLoader.getWebviewContent(LatexWebViewLoader.openedPanel.panel.webview, 
+      {extensionPath, projectName, nablaModelPath, offset});
+
+  }
+
+  constructor(panel: vscode.WebviewPanel, webViewContext: LatexWebViewContext) {
+    this.panel = panel;
+    panel.webview.html = LatexWebViewLoader.getWebviewContent(panel.webview, webViewContext);
+    panel.onDidDispose(
+      () => {
+        LatexWebViewLoader.openedPanel = undefined;
+      },
+      null,
+      []
+    );
+  }
+
+  public static getWebviewContent(webView: vscode.Webview, webViewContext: LatexWebViewContext): string {
+    // Local path to main script run in the webview
+    const reactAppPathOnDisk = vscode.Uri.file(path.join(webViewContext.extensionPath, 'nablabweb', 'nablabweb.js'));
+    const reactAppUri = webView.asWebviewUri(reactAppPathOnDisk);
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>LaTex</title>
+          <script>
+            window.acquireVsCodeApi = acquireVsCodeApi;
+            window.projectName = '${webViewContext.projectName}';
+            window.nablaModelPath = '${webViewContext.nablaModelPath}';
+            window.offset = ${webViewContext.offset};
+          </script>
+      </head>
+      <body>
+          <div id="root"></div>
+          <script src="${reactAppUri}"></script>
+      </body>
+      </html>`;
+  }
+}
+
+interface LatexWebViewContext {
+  extensionPath: string;
+  projectName: string;
+  nablaModelPath: string;
+  offset: number
+}

0c2ef64096d689f8d24dcf8f7115d8b3e15219ee
Axel RICHARD <axel.richard@obeo.fr>
2022-03-02 14:40:23 +0100

    [dev] vscode extension: add keybinding to open the jobs graph view
    Use F2, the same key than the eclipse environment
    
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- plugins/fr.cea.nabla.vscode.extension/src/extension.ts
+++ plugins/fr.cea.nabla.vscode.extension/src/extension.ts
@@ -7,15 +7,20 @@
  * SPDX-License-Identifier: EPL-2.0
  * Contributors: see AUTHORS file
  *******************************************************************************/
-import { ChildProcess, exec } from 'child_process';
-import * as net from 'net';
-import * as os from 'os';
-import * as path from 'path';
-import { commands, ExtensionContext, Uri, window, workspace } from 'vscode';
-import { Trace } from 'vscode-jsonrpc';
-import { LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo } from 'vscode-languageclient';
-import JobsGraphWebViewLoader from './view/JobsGraphWebViewLoader';
-import LatexWebViewLoader from './view/LatexWebViewLoader';
+import { ChildProcess, exec } from "child_process";
+import * as net from "net";
+import * as os from "os";
+import * as path from "path";
+import { commands, ExtensionContext, Uri, window, workspace } from "vscode";
+import { Trace } from "vscode-jsonrpc";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  StreamInfo,
+} from "vscode-languageclient";
+import JobsGraphWebViewLoader from "./view/JobsGraphWebViewLoader";
+import LatexWebViewLoader from "./view/LatexWebViewLoader";
 
 let lc: LanguageClient;
 let siriusWebServer: ChildProcess;
@@ -24,7 +29,7 @@ export function activate(context: ExtensionContext) {
   let serverOptions: ServerOptions;
   // Dev mode: In this mode the server is expected to be launched already
   // The communication is done using socket
-  if (process.env.nablaLaunchingMode == 'socket') {
+  if (process.env.nablaLaunchingMode == "socket") {
     const connectionInfo = {
       port: 5008,
     };
@@ -40,28 +45,33 @@ export function activate(context: ExtensionContext) {
     };
   } else {
     // Normal mode: The LSP server is launched from embedded jars locally installed in src/nabla
-    const launcher = os.platform() === 'win32' ? 'nabla-ls.bat' : 'nabla-ls';
-    const script = context.asAbsolutePath(path.join('src', 'nabla', 'bin', launcher));
+    const launcher = os.platform() === "win32" ? "nabla-ls.bat" : "nabla-ls";
+    const script = context.asAbsolutePath(
+      path.join("src", "nabla", "bin", launcher)
+    );
     serverOptions = {
       run: { command: script },
-      args: ['-trace'],
+      args: ["-trace"],
       debug: { command: script, args: [], options: { env: createDebugEnv() } },
     };
     // The Sirius Server is also launched from embedded jars locally installed in src/nabla
-    const siriusWebServerLauncher = os.platform() === 'win32' ? 'nabla-sirius.bat' : 'nabla-sirius';
-    const siriusWebServerScript = context.asAbsolutePath(path.join('src', 'nabla', 'bin', siriusWebServerLauncher));
+    const siriusWebServerLauncher =
+      os.platform() === "win32" ? "nabla-sirius.bat" : "nabla-sirius";
+    const siriusWebServerScript = context.asAbsolutePath(
+      path.join("src", "nabla", "bin", siriusWebServerLauncher)
+    );
 
     siriusWebServer = exec(siriusWebServerScript);
   }
   const clientOptions: LanguageClientOptions = {
-    documentSelector: ['nabla', 'nablagen'],
+    documentSelector: ["nabla", "nablagen"],
     synchronize: {
-      fileEvents: workspace.createFileSystemWatcher('**/*.*'),
+      fileEvents: workspace.createFileSystemWatcher("**/*.*"),
     },
   };
 
   // Create the language client and start the client.
-  lc = new LanguageClient('NabLab Xtext Server', serverOptions, clientOptions);
+  lc = new LanguageClient("NabLab Xtext Server", serverOptions, clientOptions);
   // enable tracing (.Off, .Messages, Verbose)
   lc.trace = Trace.Verbose;
   const disposable = lc.start();
@@ -70,70 +80,105 @@ export function activate(context: ExtensionContext) {
   // client can be deactivated on extension deactivation
   context.subscriptions.push(disposable);
 
-  const disposableLatexPanel = commands.registerCommand('nablabweb.showLatexView', () => {
-    LatexWebViewLoader.createOrShow(context.extensionPath);
-  });
+  const disposableLatexPanel = commands.registerCommand(
+    "nablabweb.showLatexView",
+    () => {
+      LatexWebViewLoader.createOrShow(context.extensionPath);
+    }
+  );
   context.subscriptions.push(disposableLatexPanel);
 
-  const disposableJobsGraphPanel = commands.registerCommand('nablabweb.showJobsGraphView', () => {
-    JobsGraphWebViewLoader.createOrShow(context.extensionPath, '');
-  });
+  const disposableJobsGraphPanel = commands.registerCommand(
+    "nablabweb.showJobsGraphView",
+    () => {
+      JobsGraphWebViewLoader.createOrShow(context.extensionPath, "");
+    }
+  );
   context.subscriptions.push(disposableJobsGraphPanel);
 
   window.onDidChangeTextEditorSelection((e) => {
-    if (e?.textEditor?.document?.languageId === 'nabla') {
+    if (e?.textEditor?.document?.languageId === "nabla") {
       const activeEditorFileURI = e.textEditor.document.uri;
       const projectFolder = workspace.getWorkspaceFolder(activeEditorFileURI);
-      const offset = e.textEditor.document.offsetAt(e.textEditor.selection?.anchor);
+      const offset = e.textEditor.document.offsetAt(
+        e.textEditor.selection?.anchor
+      );
       if (projectFolder && offset) {
-        LatexWebViewLoader.update(context.extensionPath, projectFolder.name, activeEditorFileURI.toString(), offset);
+        LatexWebViewLoader.update(
+          context.extensionPath,
+          projectFolder.name,
+          activeEditorFileURI.toString(),
+          offset
+        );
       }
     }
   });
 
   const disposableGenerateNablagen = commands.registerCommand(
-    'nablabweb.generateNablagen.proxy',
+    "nablabweb.generateNablagen.proxy",
     async (selectedFileURI: Uri) => {
       const genDir = path.dirname(selectedFileURI.fsPath);
       const projectFolder = workspace.getWorkspaceFolder(selectedFileURI);
       if (genDir && projectFolder) {
-        commands.executeCommand('nablabweb.generateNablagen', selectedFileURI.fsPath, genDir, projectFolder.name);
+        commands.executeCommand(
+          "nablabweb.generateNablagen",
+          selectedFileURI.fsPath,
+          genDir,
+          projectFolder.name
+        );
       }
     }
   );
   context.subscriptions.push(disposableGenerateNablagen);
 
   const disposableGenerateCode = commands.registerCommand(
-    'nablabweb.generateCode.proxy',
+    "nablabweb.generateCode.proxy",
     async (selectedFileURI: Uri) => {
       const ngenPath = path.dirname(selectedFileURI.fsPath);
       const projectFolder = workspace.getWorkspaceFolder(selectedFileURI);
       const projectPath = projectFolder?.uri.fsPath;
       if (ngenPath && projectPath && projectFolder) {
-        const wsPath = projectPath.substring(0, projectPath.lastIndexOf(projectFolder.name) - 1);
+        const wsPath = projectPath.substring(
+          0,
+          projectPath.lastIndexOf(projectFolder.name) - 1
+        );
         if (wsPath) {
           const ngenRelativePath = path.relative(wsPath, ngenPath);
-          commands.executeCommand('nablabweb.generateCode', selectedFileURI.fsPath, wsPath, ngenRelativePath);
+          commands.executeCommand(
+            "nablabweb.generateCode",
+            selectedFileURI.fsPath,
+            wsPath,
+            ngenRelativePath
+          );
         }
       }
     }
   );
   context.subscriptions.push(disposableGenerateCode);
 
-  const disposableGenerateIr = commands.registerCommand('nablabweb.generateIr.proxy', async (selectedFileURI: Uri) => {
-    const genDir = path.dirname(selectedFileURI.fsPath);
-    const projectFolder = workspace.getWorkspaceFolder(selectedFileURI);
-    if (genDir && projectFolder) {
-      const irModel: string | undefined = await commands.executeCommand(
-        'nablabweb.generateIr',
-        selectedFileURI.fsPath,
-        projectFolder.name
+  const disposableGenerateIr = commands.registerCommand(
+    "nablabweb.generateIr.proxy",
+    async () => {
+      const activeEditorDocumentURI = window?.activeTextEditor?.document?.uri;
+      if (!activeEditorDocumentURI) {
+        return;
+      }
+      const activeEditorDocumentPath = activeEditorDocumentURI.fsPath;
+      const projectFolder = workspace.getWorkspaceFolder(
+        activeEditorDocumentURI
       );
-      if (irModel) {
-        JobsGraphWebViewLoader.createOrShow(context.extensionPath, irModel);
+      if (projectFolder && activeEditorDocumentPath) {
+        const irModel: string | undefined = await commands.executeCommand(
+          "nablabweb.generateIr",
+          activeEditorDocumentPath,
+          projectFolder.name
+        );
+        if (irModel) {
+          JobsGraphWebViewLoader.createOrShow(context.extensionPath, irModel);
+        }
       }
     }
-  });
+  );
   context.subscriptions.push(disposableGenerateIr);
 }
 
@@ -153,7 +198,8 @@ export function deactivate() {
 function createDebugEnv() {
   return Object.assign(
     {
-      JAVA_OPTS: '-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y',
+      JAVA_OPTS:
+        "-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y",
     },
     process.env
   );

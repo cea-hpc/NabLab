b3e955835960fcc03a20ed74c22eb4d4fb9b9e24
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2022-05-23 14:55:11 +0200

    [releng] Preparation of version v0.5.2
    Bug fix in new project wizard for Arcane generation targets.
    
    Signed-off-by: Benoit LELANDAIS <benoit.lelandais@cea.fr>

--- changeversion.sh
+++ changeversion.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 #
 
-OLD_VERSION=0.5.0
-NEW_VERSION=0.5.1
+OLD_VERSION=0.5.1
+NEW_VERSION=0.5.2
 
 echo "Looking for MANIFEST.MF files"
 FIND_RES=`find . -path ./.metadata -prune -o -name "MANIFEST.MF" -print`
